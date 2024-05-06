475a3f40c6bcfcd87c9e2e3152367caa37dba2ba
eylover <chlfkrgnl@naver.com>
2024-05-06 19:38:19 +0900

    Add .gitignore

---
+++ src/main/kotlin/com/rak/rakgg/RakGgApplication.kt
@@ -0,0 +1,11 @@
+package com.rak.rakgg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RakGgApplication
+
+fun main(args: Array<String>) {
+	runApplication<RakGgApplication>(*args)
+}
---
+++ src/test/kotlin/com/rak/rakgg/RakGgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rak.rakgg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RakGgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
